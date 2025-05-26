d946d57a2c3445e8d847ee362343bd89b8fe2d4f
suzurin <128414271+suzurinx@users.noreply.github.com>
2025-05-26 23:43:04 +0900

    Log: 2025-05-26 in-order走査の構造理解と実装

--- trees/bst-key-search/bst-key-search.py
+++ trees/bst-key-search/bst-key-search.py
@@ -8,13 +8,15 @@ class BinaryTree:
 # リスト（配列）を取得してヘルパー関数を作成
 # 先頭、中間、末尾の３点を利用して再帰的にSBTを作成する
 def sortedArrayToBST(arr):
-    return sortedArrayToBSTHelper(arr, 0, len(arr) - 1) # endはインデックスにする
+    return sortedArrayToBSTHelper(arr, 0, len(arr) - 1)  # endはインデックスにする
+
 
 # ヘルパー関数
 def sortedArrayToBSTHelper(arr, start, end):
     # ベースケース(範囲外に達したらNone。これが葉ノードになる)
     # 引数に空arrが渡されても機能する（エッジケース）
-    if start > end: return None
+    if start > end:
+        return None
 
     # 中間点を見つける
     mid = (start + end) // 2
@@ -31,8 +33,15 @@ def sortedArrayToBSTHelper(arr, start, end):
     # 葉ノードから再帰的に作成される
     return BinaryTree(arr[start], left, right)
 
+
+# in-order(中順走査)でSBTを出力する
+# return文は一文というルールがある
+# リストの合成は＋で繋げるというルールを利用して再帰的処理を実装する
 def inOrderTraversal(node):
-    pass
+    if node is None:
+        return []
+    return inOrderTraversal(node.left) + [node.data] + inOrderTraversal(node.right)
+
 
 def keyExist(key, node):
     pass
