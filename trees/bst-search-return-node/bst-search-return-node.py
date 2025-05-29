a636062c147903a879de0e42a4d702ddb8ab38c1
suzurin <128414271+suzurinx@users.noreply.github.com>
2025-05-29 13:41:32 +0900

    BST探索処理の実装とテストケースの追加
    - level-orderでの二分木構築関数 toBinaryTree を実装
    - 探索関数 treeSearch を実装（keyに対応するノードを返す）
    - ノードから配列へ変換するprintTree関数を追加（level-order出力）
    - テストケースを複数追加し、正常系・異常系を検証

--- trees/bst-search-return-node/bst-search-return-node.py
+++ trees/bst-search-return-node/bst-search-return-node.py
@@ -4,6 +4,10 @@ class BinaryTree:
         self.left = left
         self.right = right
 
+
+# level-order でBSTを構築する
+# 問題文から SBT を利用できないと判断
+# ❶引数が完全な上旬の配列でない ❷中央値が取れない ❸空の枝を持つ概念がない
 def toBinaryTree(arr):
     # ベースケース
     # if arr in None
@@ -12,3 +16,77 @@ def toBinaryTree(arr):
     # None / [] / '' / 0 などの「空っぽ系」全般に幅広く「空」のケースを防ぎたいとき
     if not arr:
         return None
+
+    # 値に意味と役割を与えるために変数をつける
+    root = BinaryTree(arr[0])
+    # 次に子をつけるノードを入れるキューを作る
+    # 最初はルートしかないのでこれを入れる
+    queue = [root]
+    # 配列のインデックス1から子ノードとして利用する
+    i = 1
+
+    # 配列の終わりまで順番に子ノードに設定する
+    while i < len(arr):
+        # 今からつける子ノードの親ノードを取り出す
+        node = queue.pop(0)
+        # 左の子ノード
+        if arr[i] is not None:
+            node.left = BinaryTree(arr[i])
+            queue.append(node.left)
+        i += 1
+
+        # 右の子ノード
+        # rightという変数はないがiで状態遷移しているところがポイント
+        if i < len(arr) and arr[i] is not None:
+            node.right = BinaryTree(arr[i])
+            queue.append(node.right)
+        i += 1
+
+    return root
+
+
+# BST内でkeyを探索して、見つかればノードを返す
+def treeSearch(root, key):
+    # バリテーション
+    # (root is None にしない理由は not rootの方がカバーしている範囲が大きくこの関数を利用される場面ではこちらの方が想定されるから)
+    if not root:
+        return None
+
+    queue = [root]
+
+    while queue:
+        node = queue.pop(0)
+        if node.data == key:
+            return node
+        if node.left:
+            queue.append(node.left)
+        if node.right:
+            queue.append(node.right)
+    return None
+
+
+# 与えられたノード以下をlevel-orderで配列に変換
+def printTree(node):
+    if node is None:
+        # Noneを返してもいいが、テストケースで[]が求められている表現なので[]を出力するようにする
+        return []
+    result = []
+    queue = [node]
+    while queue:
+        current = queue.pop(0)
+        result.append(current.data if current else None)
+        if current:
+            queue.append(current.left)
+            queue.append(current.right)
+    return result
+
+
+# テストケース
+tree1 = toBinaryTree([0, -10, 5, None, -3, None, 9])
+print(printTree(treeSearch(tree1, 5)))  # → [5, None, 9]
+print(printTree(treeSearch(tree1, 20)))  # → []
+
+tree2 = toBinaryTree([5, 3, 6, 2, 4, None, 7])
+print(printTree(treeSearch(tree2, 3)))  # → [3, 2, 4]
+print(printTree(treeSearch(tree2, 5)))  # → [5, 3, 6, 2, 4, None, 7]
+print(printTree(treeSearch(tree2, 15)))  # → []
